06df567babfbe5b134aeda64a70248aa6687d09d
Ammar Salah <ammar.salah.524@gmail.com>
2020-05-03 19:54:34 +0300

    Remoing an old Example

--- Example/People/Cards/Ppl.Ca.Company.dfm
+++
@@ -1,5 +0,0 @@
-inherited PplCaCompany: TPplCaCompany
-  Caption = 'PplCaCompany'
-  PixelsPerInch = 96
-  TextHeight = 25
-end
--- Example/People/DataAccess/Ppl.Da.PhoneNumber.pas
+++
@@ -1,9 +0,0 @@
-unit Ppl.Da.PhoneNumber;
-
-interface
-
-uses ArOrm.Da.Base, ArOrm.Obj.Info, ArOrm.Consts, Ppl.Consts;
-
-implementation
-
-end.
